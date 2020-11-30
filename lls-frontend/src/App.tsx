e03b9e6c7fe1814f4dc15d416f566ab97a83055e
Olufisayo Bamidele <fisiwizy@gmail.com>
2020-11-30 23:42:52 +0100

    initialize react app

---
+++ lls-frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Hello React</h1>
+      <Button>Hello World Button</Button>
+    </div>
+  );
+}
+
+export default App;
