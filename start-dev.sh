66c8b886a069d0af516749b9eb7b0e612ae243a0
Olufisayo Bamidele <fisiwizy@gmail.com>
2020-12-05 14:04:58 +0100

    code clenaup

--- start-dev.sh
+++ start-dev.sh
@@ -6,45 +6,14 @@ pids=()
 # trap 'stop' EXIT SIGINT SIGTERM
 trap 'kill $(jobs -pr)' SIGINT
 
-# ctrlc_count=0
-
-
-# function no_ctrlc()
-# {
-#   let ctrlc_count++;
-
-#   echo
-#   if [[ $ctrlc_count == 0 ]]; then
-
-#       echo "Releasing resources..."
-#       kill -9 "$pids"
-#       # for pid in "${pids[@]}"
-#       # do
-#       #   echo "killing process with pid $pid"
-#       #   kill -9 "$pid"
-#       # done
-#       #  echo "All node processes released. Press CTRL+C to quit"
-
-#   else
-#       exit
-#   fi
-# }
-
-# trap no_ctrlc EXIT
-
-
-
-
 cd lls-backend || exit
 npm install
 npm run dev &> server.log &
-serverpid=$1
 
 cd ../lls-frontend || exit
 yarn install || npm install
 
 npm start  &> client.log &
-clientpid=$1
 
 cd .. || exit
 
